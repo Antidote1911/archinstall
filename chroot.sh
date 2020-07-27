d567fb270e50cdfd0678f2f5c9f5e744da771a45
Antidote1911 <antidote1911@gmail.com>
2020-07-27 09:41:00 +0200

    test

--- chroot.sh
+++ chroot.sh
@@ -113,7 +113,7 @@ function install_packages() {
 	su $user_name -c 'cd; git clone https://aur.archlinux.org/yay-bin.git'
 	su $user_name -c 'cd; cd yay-bin; makepkg'
 	pushd /home/$user_name/yay-bin/
-	pacman -U *.pkg.tar.xz --noconfirm --needed --noprogressbar
+	pacman -U *.pkg.tar.zst --noconfirm --needed --noprogressbar
 	popd
 	rm -rf /home/$user_name/yay-bin
 	# do a yay system update
@@ -121,7 +121,7 @@ function install_packages() {
   su $user_name -c "yay -Syyu --quiet --noconfirm"
 	#echo "Packages from the AUR can now be installed like this:"
   #echo "su $user_name -c 'yay -S --needed --noprogressbar --needed --noconfirm PACKAGE'"
-	su $user_name -c 'yay -S --needed --noprogressbar --noconfirm arsenic'
+	su $user_name -c 'yay -S --needed --noconfirm arsenic'
 
 	# Install packages
 	yay -S -y --quiet --noconfirm --mflags --skipinteg bspwm sxhkd polybar-git grub pulseaudio pulseaudio-alsa pavucontrol networkmanager network-manager-applet xf86-input-libinput mesa xorg xorg-xinit xorg-xbacklight redshift feh htop vim firefox bash-completion git acpi zathura zathura-djvu zathura-pdf-mupdf wget dmenu netctl dialog dhcpcd
